51e747a7c33cdfab9a82aa58789b552b38642131
adithaha <adit.haha@gmail.com>
2019-09-25 03:01:49 +0700

    Update config.php

--- sample-php-website/config.php
+++ sample-php-website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website - 5',
+        'name' => 'Simple PHP Website - 6',
         'nav_menu' => [
             '' => 'Home',
             'about-us' => 'About Us',
